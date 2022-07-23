014e74c9d3f16fa0527123ff11b6f2bdf350ee10
Pieterjan Geyssens <pieterjan.geyssens@hotmail.com>
2022-07-23 20:32:17 +0200

    Added basis for hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import "./App.scss";
+import Button from "./components/Button";
 import Nav from "./components/Nav";
+import logoDatabiz from "./images/client-databiz.svg";
+import logoAudiophile from "./images/client-audiophile.svg";
+import logoMeet from "./images/client-meet.svg";
+import logoMaker from "./images/client-maker.svg";
+import heroImageDesktop from "./images/image-hero-desktop.png";
+import heroImageMobile from "./images/image-hero-mobile.png";
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
+      <main>
+        <div>
+          <h1>Make remote work</h1>
+          <p>
+            Get your team in sync, no matter your location. Streamline
+            processes, create team rituals, and watch productivity soar.
+          </p>
+          <Button></Button>
+          <aside>
+            <img src={logoDatabiz} alt="Logo Databiz" />
+            <img src={logoAudiophile} alt="Logo Audiophile" />
+            <img src={logoMeet} alt="Logo Meet" />
+            <img src={logoMaker} alt="Logo Maker" />
+          </aside>
+        </div>
+        <img
+          src={window.innerWidth > 375 ? heroImageDesktop : heroImageMobile}
+          alt="Hero"
+        />
+      </main>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = () => {
+  return <button>Learn more</button>;
+};
+
+export default Button;
